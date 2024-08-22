d40b226ef88feaf37a889c8b981c3cc7bf4483fa
janna <jannakondeth5@gmail.com>
2024-08-22 11:55:42 +0530

    project setup and models creation

---
+++ server/src/models/order.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose"
+
+const orderSchema=new Schema({
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', required: true 
+     },
+    products: [
+      {
+        product: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'Product',
+             required: true },
+        quantity: { 
+            type: Number, 
+            required: true },
+      },
+    ],
+    totalPrice: { 
+        type: Number, 
+        required: true 
+    },
+    orderDate: { 
+        type: Date, 
+        default: Date.now 
+    },
+    status: {
+         type: String,
+          default: 'Pending'
+         },
+})
+
+
+export const Order=mongoose.model("Order",orderSchema)
