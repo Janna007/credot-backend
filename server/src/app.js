05910899a6fa87b7c4c9afe19e6d58333674229f
janna <jannakondeth5@gmail.com>
2024-08-27 12:58:47 +0530

    add path

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app= express()
 //configuration
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Specify the exact origin of your frontend
+    origin: 'https://credot.vercel.app/', // Specify the exact origin of your frontend
     credentials: true // Allow cookies and other credentials
   }));
 
